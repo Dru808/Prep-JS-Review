807ff00f435529a99974634395819d64dde4e5df
Andrew Tram <MyMBP@Andrews-MacBook-Pro.local>
2016-11-21 21:52:24 -1000

    finish question 3

--- js/app.js
+++ js/app.js
@@ -24,18 +24,18 @@ Console log each variable.*/
 var numberOfFloorsBurjKhalifa = 100;
 var TokyoOlympicsYear = 1999;
 
-console.log(numberOfFloorsBurjKhalifa, TokyoOlympicsYear);
+// console.log(numberOfFloorsBurjKhalifa, TokyoOlympicsYear);
 
 
 
 /*3. Variables with Boolean values
 Declare variables named lovesPrepClass, eatsDonuts, drinksGin.  Console log each variable.*/
 
-// var lovesPrepClass = true;
-// var eatsDonuts = false;
-// var drinksGin = false;
+var lovesPrepClass = true;
+var eatsDonuts = false;
+var drinksGin = false;
 
-// console.log("Loves Prep Class: " + lovesPrepClass + " Eats Doughnuts: " + eatsDonuts + " Drinks Gin: " + drinksGin); 
+console.log("Loves Prep Class: " + lovesPrepClass + " Eats Doughnuts: " + eatsDonuts + " Drinks Gin: " + drinksGin); 
 
 /*4. Variable with Null values
 Declare variables named completedCodingChallenge, traveledToSpace.  Console log each variable*/
